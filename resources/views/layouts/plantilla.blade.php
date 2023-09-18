c3dfe6fc6fbfb7d1cf71a1d7946bb0ff1e0aa23d
mmbalbas <mmbalbasgolf@gmail.com>
2023-09-18 19:44:33 +0200

    home.blade

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>"@yield('title')"</title>
+    <!-- favicon -->
+    <!-- estilos -->
 </head>
 <body>
-    <h1>Bienvenido a la pagina principal</h1>
+    <!-- header -->
+  @yield('content')
+
+    <!-- footer -->
+
+    <!-- script -->
 </body>
 </html>
