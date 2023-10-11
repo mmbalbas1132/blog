7294b9d91f7980cd8be10accfbc53be3040a65fb
mmbalbas <mmbalbasgolf@gmail.com>
2023-10-11 09:31:23 +0200

    continúo

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Rutas Web
